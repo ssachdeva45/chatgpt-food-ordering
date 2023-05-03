05111867925eb35579b10a138bf543d86bfb9a23
tanujoberoi <er.tanujoberoi@gmail.com>
2023-05-03 16:50:14 +0530

    order status updated

--- src/main/java/com/gpt/ordering/service/OrderService.java
+++ src/main/java/com/gpt/ordering/service/OrderService.java
@@ -20,15 +20,15 @@ public class OrderService {
     @Scheduled(fixedDelay = 30000) // 5 minutes in milliseconds
     public void updateOrders() {
 
-        // Fetch all orders with status "PLACED" and update status to "ACCEPTED"
-        orderRepository.findByStatus("PLACED").forEach(order -> {
-            order.setStatus("ACCEPTED");
-            orderRepository.save(order);
-        });
-
         // Fetch all orders with status "ACCEPTED" and update status to "DELIVERED"
         orderRepository.findByStatus("ACCEPTED").forEach(order -> {
             order.setStatus("DELIVERED");
+            orderRepository.save(order);
+        });
+
+        // Fetch all orders with status "PLACED" and update status to "ACCEPTED"
+        orderRepository.findByStatus("PLACED").forEach(order -> {
+            order.setStatus("ACCEPTED");
             orderRepository.save(order);
         });
     }
